e1593373c8e0ee82798693413965cc42db40f128
raneem-daoud <raneemda9@gmail.com>
2025-08-08 03:34:03 +0300

    hero

--- Graduation_project/vite-project/src/pages/Home/Home.jsx
+++ Graduation_project/vite-project/src/pages/Home/Home.jsx
@@ -1,8 +1,20 @@
-import './Home.css';
-export default function Home() {
-    return(
-        <section>
-            <h1>Home</h1>
-        </section>
+import { Link, Outlet } from "react-router-dom"
+import RDHero from "../../components/RDHero/RDHero"
+
+const Home = () => {
+    
+    return (
+        <>
+            <RDHero
+            title ={"A Digital Product Studio that will Work"}
+            description ={<>For <span> Startups </span> , <span> Enterprise leaders </span> , <span> Enterprise leaders </span> and <span> Social Good</span></>}
+            btn1 ={{Link : "/Work_page", text : "Our Works"}}
+            btn2 ={{Link : "/ContactUs_page", text : "Contact Us"}}
+            />
+
+            <Outlet/>
+        </>
     )
 }
+
+export default Home
