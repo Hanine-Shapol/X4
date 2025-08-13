81dc1a5b658640ebe84d4aea446538a50f1e0236
Lama-Moulla <lamamoulla91@gmail.com>
2025-08-13 09:28:52 +0300

    nav

--- Graduation_project/vite-project/src/App.jsx
+++ Graduation_project/vite-project/src/App.jsx
@@ -5,18 +5,32 @@ import ContactUs_page from './pages/ContactUs_page/ContactUs_page'
 import About_page from './pages/About_page/About_page'
 import Process_page from './pages/Process_page/Process_page'
 import Work_page from './pages/Work_page/Work_page'
+import LMNavBar from './component/LMNavBar/LMNavBar'
+import LMDashborad from './pages/LMDashborad/LMDashborad'
 function App() {
   return (
     <>
-{
-<Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="/About_page" element={<About_page />} />
-  <Route path="/ContactUs_page" element={<ContactUs_page />} />
-  <Route path="/Process_page" element={<Process_page />} />
-  <Route path="/Work_page" element={<Work_page />} />
-</Routes>
-}
+      <LMNavBar
+        image="/assets/images/Logo-full.svg"
+        items={[
+          { url: "/", content: "Home" },
+          { url: "/work", content: "Work" },
+          { url: "/process", content: "Process" },
+          { url: "/about", content: "About" },
+        ]}
+        btn="Contact Us"
+        dash="Dash Borad"
+      />
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About_page" element={<About_page />} />
+        <Route path="/ContactUs_page" element={<ContactUs_page />} />
+        <Route path="/Process_page" element={<Process_page />} />
+        <Route path="/Work_page" element={<Work_page />} />
+        <Route path="/lmdashborad" element={<LMDashborad />} />  
+      </Routes>
+
 
     </>
   )
