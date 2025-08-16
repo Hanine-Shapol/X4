aa2953284e1e3d9b6bfbf79630fd7df93b3c6e75
moamen-mohamad <moamen.mohamad.ln@gmail.com>
2025-08-16 17:54:01 +0300

    fix Head Of Section Component props

--- Graduation_project/vite-project/src/components/MmHeadOfSection/HeadOfSection.jsx
+++ Graduation_project/vite-project/src/components/MmHeadOfSection/HeadOfSection.jsx
@@ -1,15 +1,14 @@
 import './HeadOfSection.css';
 
-const HeadOfSection = ({ title, subtitle }) => {
+const HeadOfSection = ({ title, subtitle, bgImage }) => {
     return (
-        <div className='head-of-section'>
-            <div className="containt">
+        <div className="head-of-section" style={{ "--bg-image": `url(${bgImage})` }}>
+            <div className="content">
                 <h2>{title}</h2>
                 <p>{subtitle}</p>
             </div>
         </div>
     );
 };
-
 
 export default HeadOfSection;
